7a338947bf542ab7bc3473e9421ce5c45ee444b0
lsy0313 <03lsy13@naver.com>
2023-09-21 16:42:29 +0900

    실습4

--- main.c
+++ main.c
@@ -3,13 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-  char ch;
+    float num; // 분자
+    float den; // 분모 
+    float result; // 나누기 결과  
+  printf("분자를 입력하세요: ");
+  scanf("%f", &num);
   
-  printf("input a character : ");
-  scanf ("%c", &ch);
+  printf("분모를 입력하세요 : ");
+  scanf("%f", &den);
+  
+  result = num / den;
+  
+  printf("나누기의 결과는 %f 입니다. \n", result); 
    
-  printf("The next character of %c (%d) is %c (%d) \n", ch, ch, ch + 1, ch + 1);
-  
   system("PAUSE");
   
   return 0;
